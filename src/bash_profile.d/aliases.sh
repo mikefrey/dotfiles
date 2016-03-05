d6e4d002e685acbcadb31c4a7d89ace11e3f425b
Mike Frey <frey.mike@gmail.com>
2016-03-04 21:17:32 -0600

    zsh

--- src/bash_profile.d/aliases.sh
+++
@@ -1,9 +0,0 @@
-# aliases
-
-alias stop_nginx='sudo /opt/nginx/sbin/nginx -s stop'
-alias start_nginx='sudo /opt/nginx/sbin/nginx'
-alias restart_nginx='sudo /opt/nginx/sbin/nginx -s stop; sudo /opt/nginx/sbin/nginx'
-alias tail_log='tail -f log/development.log'
-alias g='git'
-alias be='bundle exec'
-alias json='python -mjson.tool'
